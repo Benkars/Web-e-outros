e882a3176dae8c75714d8c37d3a4879277996b14
Benkars <bernardocabral1905@gmail.com>
2022-07-18 23:29:44 -0300

    vscode

--- java/EX06C5.java
+++ java/EX06C5.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class EX06C5 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        float a,b,c,x,y;
+        float a, b, c, x, y;
         double distancia;
 
         System.out.print("Informe os valores de A, B, C da reta R e as coordenadas de X e Y: ");
-        a = teclado.nextFloat(); 
-        b = teclado.nextFloat(); 
-        c = teclado.nextFloat(); 
-        x = teclado.nextFloat(); 
-        y = teclado.nextFloat(); 
+        a = teclado.nextFloat();
+        b = teclado.nextFloat();
+        c = teclado.nextFloat();
+        x = teclado.nextFloat();
+        y = teclado.nextFloat();
 
-        distancia = (a*x + b*y +c)/(Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)));
+        distancia = (a * x + b * y + c) / (Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)));
 
         System.out.println(distancia);
 
         teclado.close();
-    }   
+    }
 }
